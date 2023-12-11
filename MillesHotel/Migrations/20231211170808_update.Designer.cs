6ab3d124380274811a88a94b5849b7d24353b228
Mille Elfver <mille.elfver98@gmail.com>
2023-12-11 18:23:36 +0100

    Changed menu texts to reflect soft delete more clearly

--- MillesHotel/Migrations/20231211170808_update.Designer.cs
+++ MillesHotel/Migrations/20231211170808_update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MillesHotel.Data;
 
@@ -11,9 +12,11 @@ using MillesHotel.Data;
 namespace MillesHotel.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231211170808_update")]
+    partial class update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
