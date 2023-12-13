d01800c0b0a3612dad7cc5b1563308252088e0ff
Mille Elfver <mille.elfver98@gmail.com>
2023-12-13 13:31:33 +0100

    Added errorhandling to InvoiceServices

--- MillesHotelLibrary/Services/InvoiceService.cs
+++ MillesHotelLibrary/Services/InvoiceService.cs
@@ -28,19 +28,24 @@ namespace MillesHotelLibrary.Services
                 if (DateTime.TryParse(Console.ReadLine(), out DateTime invoiceDue))
                 {
                     Console.Write("Enter customer ID: ");
-                    int customerId = Convert.ToInt32(Console.ReadLine());
-
-                    var newInvoice = new Invoice
+                    if (int.TryParse(Console.ReadLine(), out int customerId))
                     {
-                        InvoiceAmount = invoiceAmount,
-                        InvoiceDue = invoiceDue,
-                        IsActive = true,
-                        CustomerID = customerId
-                    };
+                        var newInvoice = new Invoice
+                        {
+                            InvoiceAmount = invoiceAmount,
+                            InvoiceDue = invoiceDue,
+                            IsActive = true,
+                            CustomerID = customerId
+                        };
 
-                    _dbContext.Invoices.Add(newInvoice);
-                    _dbContext.SaveChanges();
-                    Console.WriteLine("Invoice created successfully.");
+                        _dbContext.Invoices.Add(newInvoice);
+                        _dbContext.SaveChanges();
+                        Console.WriteLine("Invoice created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid customer ID format. Invoice not created.");
+                    }
                 }
                 else
                 {
@@ -60,26 +65,31 @@ namespace MillesHotelLibrary.Services
         {
             foreach (var showInvoice in _dbContext.Invoices)
             {
-                Console.WriteLine($"CustomerID: {showInvoice.InvoiceID}");
+                Console.WriteLine($"InvoiceID: {showInvoice.InvoiceID}");
             }
 
             Console.Write("Enter invoice ID: ");
-            int invoiceId = Convert.ToInt32(Console.ReadLine());
-
-            var invoice = _dbContext.Invoices.Find(invoiceId);
-
-            if (invoice != null)
+            if (int.TryParse(Console.ReadLine(), out int invoiceId))
             {
-                Console.WriteLine();
-                Console.WriteLine($"Invoice ID: {invoice.InvoiceID}");
-                Console.WriteLine($"Invoice Amount: {invoice.InvoiceAmount.ToString("C2") ?? "N/A"}");
-                Console.WriteLine($"Invoice Due: {invoice.InvoiceDue.ToString("yyyy-MM-dd")}");
-                Console.WriteLine($"Is Active: {invoice.IsActive}");
-                Console.WriteLine($"Customer ID: {invoice.CustomerID}");
+                var invoice = _dbContext.Invoices.Find(invoiceId);
+
+                if (invoice != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Invoice ID: {invoice.InvoiceID}");
+                    Console.WriteLine($"Invoice Amount: {invoice.InvoiceAmount.ToString("C2") ?? "N/A"}");
+                    Console.WriteLine($"Invoice Due: {invoice.InvoiceDue.ToString("yyyy-MM-dd")}");
+                    Console.WriteLine($"Is Active: {invoice.IsActive}");
+                    Console.WriteLine($"Customer ID: {invoice.CustomerID}");
+                }
+                else
+                {
+                    Console.WriteLine("Invoice not found.");
+                }
             }
             else
             {
-                Console.WriteLine("Invoice not found.");
+                Console.WriteLine("Invalid invoice ID format. Please enter a valid number.");
             }
 
             Console.WriteLine("Press any button to continue...");
@@ -109,40 +119,45 @@ namespace MillesHotelLibrary.Services
         {
             foreach (var showInvoice in _dbContext.Invoices)
             {
-                Console.WriteLine($"CustomerID: {showInvoice.InvoiceID}");
+                Console.WriteLine($"InvoiceID: {showInvoice.InvoiceID}");
             }
 
             Console.Write("Enter invoice ID to update: ");
-            int invoiceId = Convert.ToInt32(Console.ReadLine());
-
-            var invoice = _dbContext.Invoices.Find(invoiceId);
-
-            if (invoice != null)
+            if (int.TryParse(Console.ReadLine(), out int invoiceId))
             {
-                Console.Write("Enter new invoice amount: ");
-                if (double.TryParse(Console.ReadLine(), out double newInvoiceAmount))
+                var invoice = _dbContext.Invoices.Find(invoiceId);
+
+                if (invoice != null)
                 {
-                    Console.Write("Enter new invoice due date (yyyy-mm-dd): ");
-                    if (DateTime.TryParse(Console.ReadLine(), out DateTime newInvoiceDue))
+                    Console.Write("Enter new invoice amount: ");
+                    if (double.TryParse(Console.ReadLine(), out double newInvoiceAmount))
                     {
-                        invoice.InvoiceAmount = newInvoiceAmount;
-                        invoice.InvoiceDue = newInvoiceDue;
-                        _dbContext.SaveChanges();
-                        Console.WriteLine("Invoice information updated successfully.");
+                        Console.Write("Enter new invoice due date (yyyy-mm-dd): ");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime newInvoiceDue))
+                        {
+                            invoice.InvoiceAmount = newInvoiceAmount;
+                            invoice.InvoiceDue = newInvoiceDue;
+                            _dbContext.SaveChanges();
+                            Console.WriteLine("Invoice information updated successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid date format. Invoice information not updated.");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Invalid date format. Invoice information not updated.");
+                        Console.WriteLine("Invalid amount format. Invoice information not updated.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid amount format. Invoice information not updated.");
+                    Console.WriteLine("Invoice not found.");
                 }
             }
             else
             {
-                Console.WriteLine("Invoice not found.");
+                Console.WriteLine("Invalid invoice ID format. Please enter a valid number.");
             }
 
             Console.WriteLine("Press any button to continue...");
@@ -157,19 +172,24 @@ namespace MillesHotelLibrary.Services
             }
 
             Console.Write("Enter invoice ID to soft delete: ");
-            int invoiceId = Convert.ToInt32(Console.ReadLine());
-
-            var invoice = _dbContext.Invoices.Find(invoiceId);
-
-            if (invoice != null)
+            if (int.TryParse(Console.ReadLine(), out int invoiceId))
             {
-                invoice.IsActive = false;
-                _dbContext.SaveChanges();
-                Console.WriteLine("Invoice soft deleted successfully.");
+                var invoice = _dbContext.Invoices.Find(invoiceId);
+
+                if (invoice != null)
+                {
+                    invoice.IsActive = false;
+                    _dbContext.SaveChanges();
+                    Console.WriteLine("Invoice soft deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Invoice not found.");
+                }
             }
             else
             {
-                Console.WriteLine("Invoice not found.");
+                Console.WriteLine("Invalid invoice ID format. Please enter a valid number.");
             }
 
             Console.WriteLine("Press any button to continue...");
