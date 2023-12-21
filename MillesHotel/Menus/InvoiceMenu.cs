c1716c69bce6988c7fb78640a2e3e295c559c22e
Mille Elfver <mille.elfver98@gmail.com>
2023-12-21 18:36:54 +0100

    Added submenu to invoice and created 2 new methods to show paid and unpaid invoices

--- MillesHotel/Menus/InvoiceMenu.cs
+++ MillesHotel/Menus/InvoiceMenu.cs
@@ -23,16 +23,18 @@ namespace MillesHotel.Menus
             {
                 Console.Clear();
                 Message.MillesHotelMessage();
-                Console.WriteLine("╭───────────────────────╮");
-                Console.WriteLine("│Invoice Menu           │");
-                Console.WriteLine("│1. Register Invoice    │");
-                Console.WriteLine("│2. See Invoice         │");
-                Console.WriteLine("│3. See all Invoices    │");
-                Console.WriteLine("│4. Update Invoice      │");
-                Console.WriteLine("│5. Soft Delete Invoice │");
-                Console.WriteLine("│6. Register payment    │");
-                Console.WriteLine("│0. Return to MainMenu  │");
-                Console.WriteLine("╰───────────────────────╯");
+                Console.WriteLine("╭─────────────────────────────╮");
+                Console.WriteLine("│Invoice Menu                 │");
+                Console.WriteLine("│1. Register Invoice          │");
+                Console.WriteLine("│2. See Invoice               │");
+                Console.WriteLine("│3. See all Invoices          │");
+                Console.WriteLine("│4. See all paid Invoices     │");
+                Console.WriteLine("│5. See all unpaid Invoices   │");
+                Console.WriteLine("│6. Update Invoice            │");
+                Console.WriteLine("│7. Soft Delete Invoice       │");
+                Console.WriteLine("│8. Register payment          │");
+                Console.WriteLine("│0. Return to MainMenu        │");
+                Console.WriteLine("╰─────────────────────────────╯");
 
                 Console.Write("Enter your choice: ");
                 if (int.TryParse(Console.ReadLine(), out choice))
@@ -51,13 +53,63 @@ namespace MillesHotel.Menus
                             Console.ReadKey();
                             break;
                         case 4:
-                            invoiceService.UpdateInvoice();
+                            invoiceService.GetAllPaidInvoices();
+                            Console.WriteLine("Press any button to continue...");
+                            Console.ReadKey();
                             break;
                         case 5:
-                            invoiceService.SoftDeleteInvoice();
+                            invoiceService.GetAllUnpaidInvoices();
+                            Console.WriteLine("Press any button to continue...");
+                            Console.ReadKey();
                             break;
                         case 6:
+                            UpdateInvoiceMenu(invoiceService);
+                            invoiceService.UpdateInvoice();
+                            break;
+                        case 7:
+                            invoiceService.SoftDeleteInvoice();
+                            break;
+                        case 8:
                             invoiceService.RegisterPayment();
+                            break;
+                        case 0:
+                            Console.WriteLine("Returning to MainMenu...");
+                            break;
+                        default:
+                            Message.ErrorMessage("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Message.ErrorMessage("Invalid input. Please enter a number.");
+                }
+
+            } while (choice != 0);
+        }
+
+        public static void UpdateInvoiceMenu(IInvoiceService invoiceService)
+        {
+            int choice;
+
+            do
+            {
+                Console.Clear();
+                Message.MillesHotelMessage();
+                Console.WriteLine("╭─────────────────────────────╮");
+                Console.WriteLine("│Update Invoice Menu          │");
+                Console.WriteLine("│1. Change amount             │");
+                Console.WriteLine("│2. Change due date           │");
+                Console.WriteLine("╰─────────────────────────────╯");
+
+                Console.Write("Enter your choice: ");
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            break;
+                        case 2:
                             break;
                         case 0:
                             Console.WriteLine("Returning to MainMenu...");
