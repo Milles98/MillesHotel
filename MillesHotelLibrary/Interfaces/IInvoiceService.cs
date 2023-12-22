62426fb9e968b6ea1b9b7030c5244d248616e0cb
Mille Elfver <mille.elfver98@gmail.com>
2023-12-22 14:36:36 +0100

    Updated CreateInvoice and AssignInvoice to properly update and not create free floating invoices

--- MillesHotelLibrary/Interfaces/IInvoiceService.cs
+++ MillesHotelLibrary/Interfaces/IInvoiceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MillesHotelLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace MillesHotelLibrary.Interfaces
     public interface IInvoiceService
     {
         void CreateInvoice();
+        void AssignInvoiceToBooking(Invoice newInvoice);
         void GetInvoiceByID();
         void GetAllInvoices();
         void GetAllPaidInvoices();
